c8fbf12bd9e35b853dd8a50cd34a4fdfc343f133
Nicholas Flintham <nick@flinny.org>
2012-04-02 07:06:12 +0100

    Kill App Longpress Back timeout - Settings (blk_jack)

--- src/com/android/settings/AndromadusSettings.java
+++ src/com/android/settings/AndromadusSettings.java
@@ -2,22 +2,29 @@ package com.android.settings;
 
 import android.content.ContentResolver;
 import android.os.Bundle;
+import android.util.Log;
 import android.preference.CheckBoxPreference;
+import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
 
+
 import com.android.settings.R;
 import com.android.settings.SettingsFragment;
 
-public class AndromadusSettings extends SettingsFragment {
+public class AndromadusSettings extends SettingsFragment
+    implements Preference.OnPreferenceChangeListener {
+
+    private final static String TAG = AndromadusSettings.class.getSimpleName();
 
     private static final String TRACKBALL_WAKE_TOGGLE = "pref_trackball_wake_toggle";
     private static final String TRACKBALL_UNLOCK_TOGGLE = "pref_trackball_unlock_toggle";
     private static final String BUTTON_CATEGORY = "pref_category_button_settings";
     private static final String STATUSBAR_SIXBAR_SIGNAL = "pref_statusbar_sixbar_signal";
     private static final String VOLUME_LOCK_SCREEN = "pref_volume_lock_screen";
+    private static final String KILL_APP_LONGPRESS_BACK_TIMEOUT = "pref_kill_app_longpress_back_timeout";
 
     private ContentResolver mCr;
     private PreferenceScreen mPrefSet;
@@ -26,6 +33,8 @@ public class AndromadusSettings extends SettingsFragment {
     private CheckBoxPreference mTrackballUnlockScreen;
     private CheckBoxPreference mUseSixbaricons;
     private CheckBoxPreference mUseVolumeLock;
+
+    private EditTextPreference mKillAppLongpressBackTimeout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -63,11 +72,54 @@ public class AndromadusSettings extends SettingsFragment {
         mUseSixbaricons.setChecked(Settings.System.getInt(getContentResolver(),
                 Settings.System.STATUSBAR_6BAR_SIGNAL, 1) == 1);
 
+        /* Kill App Longpress Back timeout duration pref */
+        mKillAppLongpressBackTimeout = (EditTextPreference) mPrefSet.findPreference(KILL_APP_LONGPRESS_BACK_TIMEOUT);
+        mKillAppLongpressBackTimeout.setOnPreferenceChangeListener(this);
+
+
         /* Remove mTrackballWake on devices without trackballs */ 
         if (!getResources().getBoolean(R.bool.has_trackball)) {
             buttonCategory.removePreference(mTrackballWake);
             buttonCategory.removePreference(mTrackballUnlockScreen);
         }
+        if (Settings.Secure.getInt(mCr, Settings.Secure.KILL_APP_LONGPRESS_BACK, 0) == 0) {
+            buttonCategory.removePreference(mKillAppLongpressBackTimeout);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // This kinda permanently sets the summary in english & makes the definition in strings.xml useless.. should probably fix
+        mKillAppLongpressBackTimeout.setSummary("Hold down back button for " + mKillAppLongpressBackTimeout.getText() + "ms to kill a process");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        String key = preference.getKey();
+        
+        if (KILL_APP_LONGPRESS_BACK_TIMEOUT.equals(key)) {
+            try {
+                int timeout = Integer.parseInt((String) newValue);
+                if (timeout < 500 || timeout > 2000) {
+                    // Out of bounds, bail!
+                    return false;
+                }
+                Settings.System.putInt(mCr, KILL_APP_LONGPRESS_BACK_TIMEOUT, timeout);
+                mKillAppLongpressBackTimeout.setSummary("Hold down back button for " + timeout + "ms to kill a process");
+                mKillAppLongpressBackTimeout.setText(Integer.toString(timeout));
+            } finally {
+                Log.d(TAG, "Exception error on preference change.");
+                return false;
+            }
+        }
+        return true;
     }
 
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
