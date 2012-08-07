0f7a70b37297fee8789de6d9586f67d9e2c21706
Nicholas Flintham <nick@flinny.org>
2012-08-07 07:48:25 +0100

    Initial Andromadus Settings import
    Change-Id: Iae12e7f79a331c500ad32b262281576db035a73b

---
+++ src/com/android/settings/AndromadusSettings.java
@@ -0,0 +1,72 @@
+package com.android.settings;
+
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.util.Log;
+import android.preference.CheckBoxPreference;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+
+
+import com.android.settings.R;
+import com.android.settings.SettingsFragment;
+
+public class AndromadusSettings extends SettingsFragment
+    implements Preference.OnPreferenceChangeListener {
+
+    private final static String TAG = AndromadusSettings.class.getSimpleName();
+
+    private static final String TRACKBALL_WAKE_TOGGLE = "pref_trackball_wake_toggle";
+    private static final String TRACKBALL_UNLOCK_TOGGLE = "pref_trackball_unlock_toggle";
+
+    private ContentResolver mCr;
+    private PreferenceScreen mPrefSet;
+
+    private CheckBoxPreference mTrackballWake;
+    private CheckBoxPreference mTrackballUnlockScreen;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.andromadus_settings);
+
+        mPrefSet = getPreferenceScreen();
+        mCr = getContentResolver();
+
+        /* Trackball wake pref */
+        mTrackballWake = (CheckBoxPreference) mPrefSet.findPreference(
+                TRACKBALL_WAKE_TOGGLE);
+        mTrackballWake.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.TRACKBALL_WAKE_SCREEN, 1) == 1);
+        mTrackballWake.setOnPreferenceChangeListener(this);
+
+        /* Trackball unlock pref */
+        mTrackballUnlockScreen = (CheckBoxPreference) mPrefSet.findPreference(
+                TRACKBALL_UNLOCK_TOGGLE);
+        mTrackballUnlockScreen.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.TRACKBALL_UNLOCK_SCREEN, 1) == 1);
+        mTrackballUnlockScreen.setOnPreferenceChangeListener(this);
+
+        /* Remove mTrackballWake on devices without trackballs */ 
+        if (!getResources().getBoolean(R.bool.has_trackball)) {
+            mPrefSet.removePreference(mTrackballWake);
+            mPrefSet.removePreference(mTrackballUnlockScreen);
+        }
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        String key = preference.getKey();
+        
+        if (TRACKBALL_WAKE_TOGGLE.equals(key)) {
+            Settings.System.putInt(mCr, Settings.System.TRACKBALL_WAKE_SCREEN, (Boolean) newValue ? 1 : 0);
+        } else if (TRACKBALL_UNLOCK_TOGGLE.equals(key)) {
+            Settings.System.putInt(mCr, Settings.System.TRACKBALL_UNLOCK_SCREEN, (Boolean) newValue ? 1 : 0);
+        }
+        return true;
+    }
+
+}
